cdaa5c15febe2bd92ce5d7e9538bbf2a09f57e6c
Antônio Elvis <102705791+Antonio-Elvis-Dev@users.noreply.github.com>
2023-11-03 16:56:18 -0300

    tela home + serviços

--- app/src/main/java/com/example/myapplication/view/Home.kt
+++ app/src/main/java/com/example/myapplication/view/Home.kt
@@ -3,10 +3,18 @@ package com.example.myapplication.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityHomeBinding
 
 class Home : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+        val nome = intent.extras?.getString("nome")
+        binding.txtNomeUsuario.text = "Bem-Vindo(a), $nome"
     }
 }
