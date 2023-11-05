7975aa9a14b3a1fa961cffc304a8d236a82d5a7e
Antônio Elvis <102705791+Antonio-Elvis-Dev@users.noreply.github.com>
2023-11-05 17:54:18 -0300

    update

--- app/src/main/java/com/example/myapplication/model/Servicos.kt
+++ app/src/main/java/com/example/myapplication/model/Servicos.kt
@@ -2,5 +2,5 @@ package com.example.myapplication.model
 
 data class Servicos(
     val img: Int? = null,
-    val nome: String? = null
+    val nome: String? = null,
 )
--- app/src/main/java/com/example/myapplication/view/Home.kt
+++ app/src/main/java/com/example/myapplication/view/Home.kt
@@ -2,11 +2,16 @@ package com.example.myapplication.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.myapplication.R
+import com.example.myapplication.adapter.ServicosAdapter
 import com.example.myapplication.databinding.ActivityHomeBinding
+import com.example.myapplication.model.Servicos
 
 class Home : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var servicosAdapter: ServicosAdapter
+    private  val listaServicos: MutableList<Servicos> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -16,5 +21,38 @@ class Home : AppCompatActivity() {
         supportActionBar?.hide()
         val nome = intent.extras?.getString("nome")
         binding.txtNomeUsuario.text = "Bem-Vindo(a), $nome"
+
+        val recyclerViewServicos = binding.recyclerViewServicos
+        recyclerViewServicos.layoutManager = GridLayoutManager(this,2)
+        servicosAdapter = ServicosAdapter(this,listaServicos)
+        recyclerViewServicos.setHasFixedSize(true)
+
+        recyclerViewServicos.adapter = servicosAdapter
+        getServicos()
+    }
+
+    private fun getServicos(){
+        val servico1 = Servicos(R.drawable.img1,"Corte de Cabelo")
+        listaServicos.add(servico1)
+
+        val servico2 = Servicos(R.drawable.img2, "Corte de Cabelo")
+        listaServicos.add(servico2)
+
+        val servico3 = Servicos(R.drawable.img3, "Corte de Cabelo")
+        listaServicos.add(servico3)
+
+        val servico4 = Servicos(R.drawable.img4, "Corte de Cabelo")
+        listaServicos.add(servico4)
+
+
+        val servico5 = Servicos(R.drawable.img4, "Corte de Cabelo")
+        listaServicos.add(servico5)
+
+
+        val servico6 = Servicos(R.drawable.img4, "Corte de Cabelo")
+        listaServicos.add(servico6)
+
+
+
     }
 }
